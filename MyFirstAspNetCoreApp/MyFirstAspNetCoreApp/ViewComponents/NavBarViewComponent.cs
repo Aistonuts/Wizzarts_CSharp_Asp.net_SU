5e15261f8ea0cbc62f1b87a20aa6f16ec8711143
Aistonuts <120518044+Aistonuts@users.noreply.github.com>
2022-12-20 14:23:31 +0200

    navbar

---
+++ MyFirstAspNetCoreApp/MyFirstAspNetCoreApp/ViewComponents/NavBarViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstAspNetCoreApp.Services;
+using MyFirstAspNetCoreApp.ViewModels.NavBar;
+
+namespace MyFirstAspNetCoreApp.ViewComponents
+{
+    [ViewComponent(Name = "NavBar")]
+    public class NavBarViewComponent:ViewComponent
+    {
+        private readonly IYearsService yearsService;
+
+        public NavBarViewComponent(IYearsService yearsService)
+        {
+            this.yearsService = yearsService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var viewModel = new NavBarViewModel();
+            viewModel.Years = this.yearsService.GetLastYears(5);
+            return this.View(viewModel);
+        }
+    }
+
+    
+}
