404c6f580c8acc9049fd54872996e54574095cd9
Aistonuts <120518044+Aistonuts@users.noreply.github.com>
2023-01-08 11:57:57 +0200

    Cm1

--- ForumSystem/Web/ForumSystem.Web/Program.cs
+++ ForumSystem/Web/ForumSystem.Web/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ForumSystem.Web
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ForumSystem.Data;
+namespace ForumSystem.Web
 {
     using System.Reflection;
 
@@ -26,9 +29,17 @@
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+                        var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+                                    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+                                                builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             ConfigureServices(builder.Services, builder.Configuration);
             var app = builder.Build();
             Configure(app);
+                        app.UseAuthentication();;
             app.Run();
         }
 
