41c186127042f19d752783d84e03c484b4e16252
Aistonuts <120518044+Aistonuts@users.noreply.github.com>
2023-02-03 14:09:26 +0200

    InitialCommit

---
+++ SUS/MyFirstMvcApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using SUS.HTTP;
+using System;
+
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHttpServer server = new HttpServer();
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/about", About);
+            server.AddRoute("/users/login", Login);
+
+            await server.StartAsync(80);
+
+            static HttpResponse HomePage(HttpRequest request)
+            {
+                throw new NotImplementedException();
+            }
+
+            static HttpResponse About(HttpRequest request)
+            {
+                throw new NotImplementedException();
+            }
+
+            static HttpResponse Login(HttpRequest request)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
