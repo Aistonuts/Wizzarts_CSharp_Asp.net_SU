1d9804806d18d214edfbe1f64ee7434007ce3598
Aistonuts <120518044+Aistonuts@users.noreply.github.com>
2023-07-01 16:00:17 +0300

    removed

--- MagicCardsAssembly/CardsAssembler.Domain/CreatorRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace CardsAssembler.Domain
-{
-    public class CreatorRole : IdentityRole
-    {      
-    }
-}
--- MagicCardsAssembly/CardsAssembler/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CardsAssembler.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
