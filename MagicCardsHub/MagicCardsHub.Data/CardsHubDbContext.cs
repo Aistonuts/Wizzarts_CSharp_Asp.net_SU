3ce59837d3199498e2132962b4610499654240a7
Aistonuts <120518044+Aistonuts@users.noreply.github.com>
2023-07-02 12:25:32 +0300

    MagicCardsHubDataBase
    MagicCardsHubDataBase is Ready

---
+++ MagicCardsHub/MagicCardsHub.Data/CardsHubDbContext.cs
@@ -0,0 +1,71 @@
+﻿using MagicCardsHub.Data;
+using MagicCardsHub.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace MagicCardsHub.Data
+{
+    public class CardsHubDbContext : IdentityDbContext<ProjectCreator, CreatorRole, string>
+    {
+        public DbSet<Art> Arts { get; set; }
+
+        public DbSet<Card> Cards { get; set; }
+
+        public DbSet<ProjectStatus> ProjectStatuses { get; set; }
+
+        public DbSet<GameSeason> GameSeasons { get; set; }
+
+        public CardsHubDbContext(DbContextOptions<CardsHubDbContext> options) : base(options)
+        {
+        }
+
+        public CardsHubDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-PJE8JOP\SQLEXPRESS;Database=MagicCardsHubDb;TrustServerCertificate=true;Trusted_Connection=true;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ProjectCreator>()
+                .HasKey(user => user.Id);
+
+            builder
+                .Entity<Art>()
+                .HasOne(c => c.Artist)
+                .WithMany(c => c.ArtPieces)
+                .HasForeignKey(c => c.ArtIstId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Card>()
+                .HasOne(c => c.GameSeason)
+                .WithMany(d => d.Cards)
+                .HasForeignKey(c => c.GameSeasonId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<GameSeason>()
+                .HasOne<ProjectCreator>()
+                .WithMany(d => d.GameSeasons)
+                .HasForeignKey(c => c.ProjectCreatorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Art>()
+                .HasOne(art => art.Card)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
+
+
---
+++ MagicCardsHub/MagicCardsHub.Domain/ProjectCreator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MagicCardsHub.Domain
+{
+    
+    public class ProjectCreator : IdentityUser
+    {
+        public ProjectCreator()
+        {
+            this.GameSeasons = new List<GameSeason>();
+            this.ArtPieces = new List<Art>();
+        }
+
+        public ICollection<GameSeason> GameSeasons { get; set; }
+        public ICollection<Art> ArtPieces { get; set; }
+    }
+}
